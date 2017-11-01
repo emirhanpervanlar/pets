71011537bea144eb30684bc068e6487b8a9417d3
Emirhan <pervanlaremirhan@gmail.com>
2017-11-01 19:12:05 +0300

    Push all

--- rest_test2.php
+++ rest_test2.php
@@ -4,11 +4,11 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
    http_response_code(200);
 }else if($_SERVER['REQUEST_METHOD'] == "POST"){
     echo "POST kullanıldı";
-    if(isset($_POST['name'])){
+
         include("rest_test.php");
         deliver_responsive(100,'book found','deneme');
         http_response_code(200);
-    }
+
 
 }else {
     http_response_code(405);
